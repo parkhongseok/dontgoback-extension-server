f23e14fdb82d93bd5cd95ef2e3f1fa3897e805f0
parkhongseok <123qkrghdtjr@naver.com>
2025-08-03 17:22:55 +0900

    init: new project setting

---
+++ dg-extension-server/src/main/java/com/dontgoback/msa/extension/DgExtensionServerApplication.java
@@ -0,0 +1,13 @@
+package com.dontgoback.msa.extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgExtensionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgExtensionServerApplication.class, args);
+	}
+
+}
